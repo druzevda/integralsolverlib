c90377158926365dbea137416065108c1837b680
druzevda <druzev1@mail.ru>
2023-04-06 20:52:09 +0600

    make better tests

--- include/tests.hxx
+++ include/tests.hxx
@@ -5,18 +5,33 @@ template<typename T = double>
 constexpr bool are_equal(const T lhs, const T rhs, const T eps = 1e-7){
     return std::abs(lhs-rhs) < eps;
 }
-template<typename T = double>
-void test(const IntegralSolver<T>& solver);
 
-template<typename T = double>
-void test(const IntegralSolver<T>& solver){
-    constexpr T a1 = 0.0;
-    constexpr T b1 = 1.0;
-    const auto func1     = []     (const T x){return x                ;};
-    const auto exact1    = [a1,b1](         ){return (b1*b1-a1*a1)/2.0;};
-    const T    calc1 = solver.get_solution(func1,a1,b1);
-    const T    real1 = exact1();
-    assert(are_equal(calc1,real1));
-    std::cout << "\tTest1+" << std::endl;
-}
+template<
+    typename ArgT = double,
+    typename RetT = double
+    >
+class IntegralTest{
+  using integratedFunction = std::function<RetT(ArgT)>;
+  using exactSolution      = std::function<RetT(ArgT,ArgT)>;
+  const integratedFunction integrFunct;
+  const exactSolution      exSolution;
+
+  public:
+  IntegralTest(const integratedFunction& iF, const exactSolution& eS)
+    :integrFunct{iF}
+    ,exSolution{eS}
+    {}
+
+  bool operator()(const IntegralSolver<ArgT,RetT>& solver, const ArgT& left, const ArgT& right){
+    const RetT calced = solver.get_solution(integrFunct,left,right);
+    const RetT exact = exSolution(left,right);
+    return are_equal(calced,exact);
+  }
+
+  IntegralTest()=delete;
+  IntegralTest(const IntegralTest&)=delete;
+  IntegralTest(IntegralTest&&)=delete;
+  IntegralTest& operator=(const IntegralTest&)=delete;
+  IntegralTest& operator=(IntegralTest&&)=delete;
+};
 #endif // INTEGRAL_TESTS_INCLUDE_HXX
--- test.cpp
+++ test.cpp
@@ -9,14 +9,29 @@ int main(){
     using TrapSolver = TrapezoidRule<ArgT,RetT>;
     using SimpsSolver = SimpsonRule<ArgT,RetT>;
 
+    std::vector<IntegralSolver<ArgT,RetT>*> solvers;
+
     RecSolver   rsolver;
     TrapSolver  tsolver;
     SimpsSolver ssolver;
 
-    std::cout << "Rectangle rule:" << std::endl;
-    test(rsolver);
-    std::cout << "Trapezoid rule:" << std::endl;
-    test(tsolver);
-    std::cout << "Simpson rule:" << std::endl;
-    test(ssolver);
+    solvers.push_back(&rsolver);
+    solvers.push_back(&tsolver);
+    solvers.push_back(&ssolver);
+
+    using integratedFunction = IntegralTest<ArgT,RetT>::integratedFunction;
+    using exactSolution      = IntegralTest<ArgT,RetT>::exactSolution;
+
+    const integratedFunction iF= []([[maybe_unused]]const ArgT& x)->RetT{
+        return 1;
+    };
+    const exactSolution eS = [](const ArgT& a, const ArgT& b)->RetT{
+        return b-a;
+    };
+
+    IntegralTest test1{iF,eS};
+    for(const auto& solver : solvers){
+      assert(test1(*solver,0,1));
+    }
+
 }
